5ec5056376c2e07a2abdb838e5b37403107e19eb
Raymond Mak <raymond1.ym2.mak3@gmail.com>
2024-10-22 16:16:59 +0800

    Initial Commit

---
+++ Samsung AI/Samsung_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Samsung_AIApp.swift
+//  Samsung AI
+//
+//  Created by Raymond Mak on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Samsung_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Samsung AITests/Samsung_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Samsung_AITests.swift
+//  Samsung AITests
+//
+//  Created by Raymond Mak on 22/10/2024.
+//
+
+import Testing
+@testable import Samsung_AI
+
+struct Samsung_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
